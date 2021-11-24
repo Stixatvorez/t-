6301016a66f1f3c55ade1133259aaa1544cff0b8
opilane <opilane@HP-CZC6297B9B>
2021-11-24 12:24:07 +0200

    töö

--- djadja_volodja.py
+++ djadja_volodja.py
@@ -1,1 +1,36 @@
+import random
+#Cоздаем функции
+def passautomat()->str:
+	"""Пароль создается машиной
+	"""	
+	str0=".,:;!_*-+()/#¤%&"
+	str1 = '0123456789'
+	str2 = 'qwertyuiopasdfghjklzxcvbnm'
+	str3 = str2.upper() # 'QWERTYUIOPASDFGHJKLZXCVBNM'
+	str4 = str0+str1+str2+str3
+	ls = list(str4) # список [".",",",":"...]
+	random.shuffle(ls) #перемешаем
+# Извлекаем из списка 12 произвольных значений
+	psword = ''.join([random.choice(ls) for x in range(12)])
+# Пароль готов
+	return psword
+def paskontroll(psword: str)->bool:
+	ls=list(psword)
+	for e in ls:
+		if e.isdigit(): d=True
+		if e.isalpha(): a=True
+		if e.isupper(): u=True
+		if e.islower():l=True
+		if e in list(".,:;!_*-+()/#¤%&"): s=True
+	if d==True and a==True and u==True and l==True and s==True:
+		t=True
+	else:
+		t=False
+	return t
+def koik_kasutajad(users,passwords):
+	i=0
+	for user in users:
+		print(user,end="-")
+		print(passwords[i])
+		i+=1
 
